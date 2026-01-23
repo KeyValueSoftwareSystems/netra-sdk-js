01e0158300ad0f18aaa0df3a199ae447e03a4ab7
Ashish S Kumar <ashish@keyvalue.systems>
2026-01-23 12:26:59 +0530

    Removing unwanted file.

--- verify_patch.ts
+++
@@ -1,7 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-import { googleGenAIInstrumentor } from "./src/instrumentation/google-genai/index.ts";
-
-const client = new GoogleGenAI({
-  apiKey: "AIzaSyALq4VgvfhF6dLVC-RDH5jKH3Pl2AjwY98",
-});
-console.log(JSON.stringify(GoogleGenAI.prototype.models));
