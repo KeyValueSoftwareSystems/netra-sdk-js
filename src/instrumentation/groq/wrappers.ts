f0d4a91dfba428b4797e91646aa22f677bcddd95
Akash Vijay <akash@keyvalue.systems>
2026-06-08 18:41:41 +0530

    [NET-1053] fix: Make streaming wrapper OTel properties non-enumerable to prevent circular JSON serialization (#71)

--- src/instrumentation/groq/wrappers.ts
+++ src/instrumentation/groq/wrappers.ts
@@ -2,6 +2,7 @@ import { Tracer, Span, SpanKind, SpanStatusCode, context } from "@opentelemetry/
 import { Logger } from "../../logger";
 import { setRequestAttributes, setResponseAttributes } from "./utils";
 import {
+  defineHidden,
   modelAsDict,
   isPromise,
   shouldSuppressInstrumentation,
@@ -146,13 +147,22 @@ export const chatWrapper = (tracer: Tracer) =>
 export class StreamingWrapper implements Iterable<unknown>, Iterator<unknown> {
   private iterator: Iterator<unknown> | null = null;
   private completeResponse: Record<string, any> = { choices: [], model: "" };
+  // Assigned via defineHidden in constructor (non-enumerable to avoid circular JSON)
+  private span!: Span;
+  private response!: any;
+  private startTime!: number;
+  private requestKwargs!: Record<string, any>;
 
-  constructor(
-    private span: Span,
-    private response: any,
-    private startTime: number,
-    private requestKwargs: Record<string, any>
-  ) {}
+  constructor(span: Span, response: any, startTime: number, requestKwargs: Record<string, any>) {
+    defineHidden(this, "span", span);
+    defineHidden(this, "response", response);
+    defineHidden(this, "startTime", startTime);
+    defineHidden(this, "requestKwargs", requestKwargs);
+  }
+
+  toJSON() {
+    return this.completeResponse;
+  }
 
   [Symbol.iterator](): Iterator<unknown> {
     return this;
@@ -266,13 +276,22 @@ export class AsyncStreamingWrapper
     choices: [],
     model: "",
   };
+  // Assigned via defineHidden in constructor (non-enumerable to avoid circular JSON)
+  private span!: Span;
+  private response!: any;
+  private startTime!: number;
+  private requestKwargs!: Record<string, any>;
 
-  constructor(
-    private span: Span,
-    private response: any,
-    private startTime: number,
-    private requestKwargs: Record<string, any>
-  ) {}
+  constructor(span: Span, response: any, startTime: number, requestKwargs: Record<string, any>) {
+    defineHidden(this, "span", span);
+    defineHidden(this, "response", response);
+    defineHidden(this, "startTime", startTime);
+    defineHidden(this, "requestKwargs", requestKwargs);
+  }
+
+  toJSON() {
+    return this.completeResponse;
+  }
 
   [Symbol.asyncIterator](): AsyncIterator<unknown> {
     return this;
